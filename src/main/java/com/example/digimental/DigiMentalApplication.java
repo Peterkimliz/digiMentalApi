59d37ddeaa2bb621de63a665810b74a5165f218c
Freddy <githumbi3fred@gmail.com>
2023-03-16 05:45:00 +0300

    added the folder structure

---
+++ src/main/java/com/example/digimental/DigiMentalApplication.java
@@ -0,0 +1,13 @@
+package com.example.digimental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigiMentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigiMentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/digimental/DigiMentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.digimental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigiMentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
