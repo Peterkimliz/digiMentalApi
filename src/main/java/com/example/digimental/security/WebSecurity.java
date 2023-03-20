b71c2f141f9c08a92e95205ab85c166bb03e2389
Freddy <githumbi3fred@gmail.com>
2023-03-20 17:38:46 +0300

    added patient endpoint

--- src/main/java/com/example/digimental/security/WebSecurity.java
+++ src/main/java/com/example/digimental/security/WebSecurity.java
@@ -15,6 +15,7 @@ public class WebSecurity {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+
                 .authorizeHttpRequests()
                 .requestMatchers("/api/categories/all/**","/api/categories/category/**")
                 .permitAll()
