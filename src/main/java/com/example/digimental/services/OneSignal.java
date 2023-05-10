355b0d8e7c1c0043b9b23dfa94208f01edd910ae
peterKim <51915958+Peterkimliz@users.noreply.github.com>
2023-05-10 17:54:47 +0300

    changed app id and server id one signal

--- src/main/java/com/example/digimental/services/OneSignal.java
+++ src/main/java/com/example/digimental/services/OneSignal.java
@@ -12,9 +12,9 @@ public class OneSignal {
         try {
             HttpResponse<String> response = Unirest.post("https://onesignal.com/api/v1/notifications")
                     .header("accept", "application/json")
-                    .header("Authorization", "Basic NTY0MTJhOGMtYTE1OS00ZmZlLTk0NjAtOTg1ZjU4NzdkY2U2")
+                    .header("Authorization", "Basic Zjk2ODFkNWMtMGFhMi00NjUyLTk0MzgtMjA1ODA5MTQ3ZTMw")
                     .header("content-type", "application/json")
-                    .body("{\"included_segments\":[\"Subscribed Users\"],\"contents\":{\"en\":\"Hello this is spring application\",\"es\":\"Spanish Message\"},\"name\":\"INTERNAL_CAMPAIGN_NAME\",\"app_id\":\"a10568db-f14b-4823-9142-73f54651b395\",\"data\":{\"abc\": 123, \"foo\": \"bar\", \"event_performed\": true, \"amount\": 12.1}}")
+                    .body("{\"included_segments\":[\"Subscribed Users\"],\"contents\":{\"en\":\"Hello this is spring application\",\"es\":\"Spanish Message\"},\"name\":\"INTERNAL_CAMPAIGN_NAME\",\"app_id\":\"6e2af418-ffae-4555-a74b-a4092781d3ca\",\"data\":{\"abc\": 123, \"foo\": \"bar\", \"event_performed\": true, \"amount\": 12.1}}")
                     .asString();
             System.out.println("response is "+response.getBody());
         } catch (UnirestException e) {
